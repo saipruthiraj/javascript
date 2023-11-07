0e344ae40131fa392ce6b35a25240b443458a1dc
Sai Pruthiraj <nayaksaipruthiraj@gmail.com>
2023-11-07 10:40:46 +0530

    first commit

---
+++ variableConstant.js
@@ -0,0 +1,11 @@
+console.log("SAI")
+const accountID=12000
+console.log(accountID)
+let accountEmail="sai@gmail.com"
+var parrword="sai@2002"
+accountCity="bhubaneswar" //without taking type we  declair variable
+let money
+//accountID=2
+console.log(accountID)
+accountEmail="saiii@gmail.com"
+console.table([accountID,accountEmail,accountCity,parrword,money])
