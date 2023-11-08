043e7ec42226d2fa27013a10bf6211a7a9c6b12f
saipruthiraj <145189719+saipruthiraj@users.noreply.github.com>
2023-11-08 06:48:41 +0000

    object code

---
+++ DatatypeConversion.js
@@ -0,0 +1,30 @@
+let score="33"
+console.log(typeof score)
+let valueinnumber =Number(score)
+console.log(valueinnumber);
+
+
+let name=null
+let value=Number(name)
+console.log(value)
+console.log(typeof NaN)
+
+
+//"33"=>33
+//"33abs"=>NaN
+//null =>0
+
+//boolean conversion
+let a="0"
+let bool=Boolean(a);
+console.log(bool)
+
+//undefined=>false
+//null=>false
+//any number except 0=>true
+//0=false
+//""=>false
+//or else is true
+let str=String(bool)
+console.log(str)
+
